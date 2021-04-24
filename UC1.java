eb79eca17aec5c6c2fca952372312df4fbeb94bb
06kaushik <himanshu06kaushik@gmail.com>
2021-04-24 08:21:46 +0530

    single_player_start_position

---
+++ UC1.java
@@ -0,0 +1,10 @@
+package day4;
+
+public class snakeLadder {
+	 public static void main(String[] args) {
+         System.out.println("Welcome to snake and ladder game");
+	int startPosition = 0;
+	System.out.println("player starts at position " + startPosition);
+}
+
+}
